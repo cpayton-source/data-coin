a0fbd10a0c21f3b81c8aa4be7392875b318a7cb2
Can Dilikoglu <55206720+candilikoglu@users.noreply.github.com>
2022-10-27 22:15:19 -0400

    first commit
    first commit

---
+++ csc404.datacoin/src/main/java/com/cryptoccurency/csc404/datacoin/App.java
@@ -0,0 +1,13 @@
+package com.cryptoccurency.csc404.datacoin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
