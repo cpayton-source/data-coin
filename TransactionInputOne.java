64898f675b7b9c108f315ebbcb439ef7b4e8a55b
Cameron Payton <cameron.payton@my.hamptonu.edu>
2022-10-30 13:04:49 -0400

    Create TransactionInput File
    This is the interface for the transaction input file. Added a one to the end of the file name to differentiate between my version and original.

---
+++ TransactionInputOne.java
@@ -0,0 +1,13 @@
+package com.dottorsoft.SimpleBlockChain.core;
+
+public class TransactionInputOne{
+   
+   public TransactionInputOne(){
+   
+   }
+  public TransactionInputOne(String transactionOutputOneId) {
+
+	}
+}
+
+}
